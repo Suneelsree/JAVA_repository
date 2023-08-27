71db83955e54a75fba7703cc0a1f121303d0b844
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-08-27 22:07:15 +0530

    Update and rename Progra_1.txt to FindAscii.java
    1

---
+++ FindAscii.java
@@ -0,0 +1,14 @@
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        //Decleration && intiallization
+        char ch = 'a';
+        int ascii = ch;
+         //type casting
+        int castAscii = (int) ch;
+        //displaying output
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
