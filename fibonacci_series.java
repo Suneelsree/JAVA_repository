c9fbca5f007da32f9ebb5e4d9d1f5a208f08b766
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-08-27 22:16:14 +0530

    Create fibonacci_series.java

---
+++ fibonacci_series.java
@@ -0,0 +1,19 @@
+public class Main {
+  public static void main(String[] args) {
+    //Declaration  && initiallization 
+    int n , a = 0, b = 1;
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter limit term"): 
+    n=s.nextInt();
+    
+    System.out.println("Fibonacci Series till " + n + " terms:");
+    //for loop
+    for (int i = 1; i <= n; ++i) {
+      //display series
+      System.out.println(a + ", ");
+      int c = a + b;
+      a = b;
+      b = c;
+    }
+  }
+}
