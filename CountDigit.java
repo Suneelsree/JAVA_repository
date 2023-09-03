8134400512993bd2b88c61c85c93c244800ff3dc
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-09-03 21:23:45 +0530

    Insert CountDigit.java

---
+++ CountDigit.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int count = 0;
+    int num;
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter number :");
+    num=s.nextInt();
+    while (num != 0) {
+      num /= 10;
+      ++count;
+    }
+    System.out.println("Number of digits: " + count);
+  }
+}
