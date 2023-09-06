d1e93e47670df3aa714ec793a33bf79098fe0817
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-09-06 22:45:43 +0530

    Update Factor.java

---
+++ Factor.java
@@ -0,0 +1,14 @@
+public class Factor {
+  public static void main(String[] args) {
+    int number;
+    Scanner s=new Scanner(System.in);
+    System.out.print("Enter number");
+    number=s.nextInt();
+    System.out.print("Factors of " + number + " are: ");
+    for (int i = 1; i <= number; ++i) {
+      if (number % i == 0) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+}
