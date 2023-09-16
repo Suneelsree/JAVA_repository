9509dd5081d5a0546b9f53903f143627d90007a8
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-09-16 22:08:06 +0530

    Create PyramidNum.java

---
+++ PyramidNum.java
@@ -0,0 +1,13 @@
+public static void main(String[] args) {
+    int rows;
+    Scanner s=new Scanner(System.in);
+    System.out.println("enter rows");
+    rows=s.nextInt();
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print(j + " ");
+      }
+      System.out.println();
+    }
+  }
+}
