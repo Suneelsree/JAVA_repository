8cd98f2890c0cfd8b34c3673297d6f7d51237a4d
Suneelsree <suneel28122003@gmail.com>
2023-09-23 08:30:47 +0530

    Type casting in narrow way

---
+++ TypeNarrowCasting.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    double myDouble = 4.99d;
+    int myInt = (int) myDouble;
+    System.out.println(myDouble);
+    System.out.println(myInt);
+  }
+}
