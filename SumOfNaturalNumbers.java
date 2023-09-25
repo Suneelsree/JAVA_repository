98b7fbcf665ae37aca9eb005c77f536855d86fce
Suneelsree <suneel28122003@gmail.com>
2023-09-25 19:29:09 +0530

    total sum of given natural number

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,15 @@
+class SumNatural {
+
+    public static void main(String[] args) {
+
+        int num, sum = 0;
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter number");
+        num=s.nextInt();
+        for(int i = 1; i <= num; ++i)
+        {
+            sum += i;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
