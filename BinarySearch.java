1e36c31340a829f14f29759fd84be892a39adf86
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-10-12 21:54:37 +0530

    Insert BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,35 @@
+class BinarySearch{  
+ public static void binarySearch(int arr[], int first, int last, int key){  
+   int mid = (first + last)/2;  
+   while( first <= last ){  
+      if ( arr[mid] < key ){  
+        first = mid + 1;     
+      }else if ( arr[mid] == key ){  
+        System.out.println("Element is found at index: " + mid);  
+        break;  
+      }else{  
+         last = mid - 1;  
+      }  
+      mid = (first + last)/2;  
+   }  
+   if ( first > last ){  
+      System.out.println("Element is not found!");  
+   }  
+ }  
+ public static void main(String args[]){  
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter no of elements");
+         int n=s.nextInt();
+        int arr[n] =new int();  
+        System.out.println("enter Element one by one:");
+        for(int i=0;i<n;i++)
+        {
+          a[i]=s.nextInt();
+        }
+        int key;
+        System.out.println("enter Search elements");
+        key=s.nextInt(); 
+        int last=arr.length-1;  
+        binarySearch(arr,0,last,key);     
+ }  
+}  
