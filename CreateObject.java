d82419d8ce64a7c7a4085f5c536500d8d32a97bd
Suneelsree Nallasamy <142733986+Suneelsree@users.noreply.github.com>
2023-10-16 20:55:37 +0530

    Insert CreateObject.java

---
+++ CreateObject.java
@@ -0,0 +1,12 @@
+public class CreateObject   
+{    
+void show()    
+{    
+System.out.println("Welcome to javaTpoint");    
+}    
+public static void main(String[] args)   
+{       
+CreateObject obj = new CreateObject();     
+obj.show();    
+}    
+} 
